7f4090cba6fb6e799642f1868ff550bc7c8fb670
AustinComet <reveno415066@gmail.com>
2018-10-15 19:55:34 +0800

    new file: Carryconversion.py

---
+++ Carryconversion.py
@@ -0,0 +1,9 @@
+inputvalue = 0 #Variable initializationb
+hexvalue = 0
+binvalue = 0
+inputvalue = input("Please enter the decimal value:") #Numerical input
+hexvalue = hex(inputvalue) #Conversion Hex
+binvalue = bin(inputvalue) #Conversion Bin
+print("Hex:" + hexvalue)
+print("binary:" + binvalue)
+
